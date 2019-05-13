af774b1d8abe2bc13dd2a9018619951a261fd6cb
Étienne Michon <etienne@scalingo.com>
2019-05-13 16:35:25 +0200

    Add an autoscaler to container_types example

--- container_types/main.tf
+++ container_types/main.tf
@@ -20,3 +20,12 @@ resource "scalingo_container_type" "default" {
   size   = "L"
   amount = "2"
 }
+
+resource "scalingo_autoscaler" "app_autoweb" {
+  app            = "${scalingo_app.sc_app.id}"
+  min_containers = 2
+  max_containers = 4
+  metric         = "rpm_per_container"
+  target         = 10
+  container_type = "web"
+}
